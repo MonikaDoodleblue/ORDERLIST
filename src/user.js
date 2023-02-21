dcecb07abd8a286f6fd04cbbcb77796c02f2d668
MonikaDoodleblue <123928993+MonikaDoodleblue@users.noreply.github.com>
2023-02-21 17:04:06 +0530

    delete

--- src/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./userController');
-const { auth } = require('../jwt');
-
-router.post('/login', controller.loginUser);
-router.post('/logout', auth, controller.logoutUser);
-
-module.exports = router;
