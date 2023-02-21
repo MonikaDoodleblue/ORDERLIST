c8d2b172fe594ad00f73c5fa0eb10a10b5763453
MonikaDoodleblue <123928993+MonikaDoodleblue@users.noreply.github.com>
2023-02-21 17:02:58 +0530

    updated

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 const apiRoutes = (app) => {
     app.use('/api', require('../src/orders/order'));
     app.use('/api', require('../src/products/product'));
+    app.use('/api', require('../src/users/user'));
 }
 
 module.exports = {
